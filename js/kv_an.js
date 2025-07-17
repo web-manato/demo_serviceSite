af6a3c150dc2f75b87b51a755a9db32ff947b722
web.manato@gmail.com <web.manato@gmail.com>
2025-07-18 07:46:40 +0900

    perf- LCP改善のためのアニメーション最適化

--- js/kv_an.js
+++ js/kv_an.js
@@ -5,7 +5,11 @@ var mainCopy = document.querySelectorAll('.bl_kv_mainCopy');
 var listItem = document.querySelectorAll('.bl_kv_function_listItem');
 var btn = document.querySelectorAll('.el_btn');
 
-tl.from(mainCopy, {
+tl.from(img, {
+        autoAlpha: 0,
+        duration: 0.6,
+    })
+    .from(mainCopy, {
         delay: 0.5,
         autoAlpha: 0,
         duration: 0.8,
@@ -16,10 +20,6 @@ tl.from(mainCopy, {
         ease: 'Power3.easeOut',
         duration: 0.5,
         stagger: 0.3,
-    })
-    .from(img, {
-        autoAlpha: 0,
-        duration: 0.6,
     })
     .to(btn, {
         y: 0,
